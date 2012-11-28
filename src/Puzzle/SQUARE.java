2ee832c4cb92c77a2a4219ce83d3fde5cdd306bd
Blaed <blaedj@gmail.com>
2012-11-27 22:45:08 -0600

    Makes SQUARE enum a seperate public class, updates .gitignore

---
+++ src/Puzzle/SQUARE.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Puzzle;
+
+/**
+ *
+ * @author blaed
+ */
+public enum SQUARE {
+
+        ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, BLANK
+}
